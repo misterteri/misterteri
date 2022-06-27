cf58ffd4daf9465922fe0aa7cfaad591abb66338
misterteri <marcelinogilbert26@gmail.com>
2022-06-27 16:28:34 +0800

    Add: Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,24 @@
-import { Flex, Link, useColorModeValue } from "@chakra-ui/react";
+import { Flex, IconButton, Link, useColorModeValue } from "@chakra-ui/react";
 import NextLink from "next/link";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { AiTwotoneMail } from "react-icons/ai";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { MdOutlineMail } from "react-icons/md";
 
 const Footer: React.FC = () => {
   const size = "2rem";
   const link = [
     {
-      icon: <BsGithub style={{ fontSize: size }} />,
+      key: "github",
+      icon: <FiGithub style={{ fontSize: size }} />,
       url: "https://github.com/misterteri",
     },
     {
-      icon: <BsLinkedin style={{ fontSize: size }} />,
+      key: "linkedin",
+      icon: <FiLinkedin style={{ fontSize: size }} />,
       url: "https://www.linkedin.com/in/marcelino-gilbert-07823621a",
     },
     {
-      icon: <AiTwotoneMail style={{ fontSize: size }} />,
+      key: "email",
+      icon: <MdOutlineMail style={{ fontSize: size }} />,
       url: "mailto:marcelinogilbert26@gmail.com",
     },
   ];
@@ -24,20 +27,12 @@ const Footer: React.FC = () => {
     <Flex as="footer" marginBottom={20} direction="column" align="center">
       <Flex maxW="container.md">
         {link.map((item, index) => (
-          <Link
-            key={index}
-            as="a"
-            px={4}
-            py={2}
-            rounded={"md"}
-            _hover={{
-              textDecoration: "none",
-              bg: useColorModeValue("gray.200", "gray.700"),
-            }}
-          >
-            <NextLink href={item.url} passHref={true}>
-              {item.icon}
-            </NextLink>
+          <Link key={index} as="a" px={4} py={2} rounded={"md"} href={item.url}>
+            <IconButton
+              variant="ghost"
+              icon={item.icon}
+              aria-label={item.key}
+            />
           </Link>
         ))}
       </Flex>
@@ -49,3 +44,5 @@ const Footer: React.FC = () => {
 };
 
 export default Footer;
+
+// UNDERSTOOD
