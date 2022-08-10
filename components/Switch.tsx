f4ee82f656afd945fe1e123e7bf86068e981b1b3
misterteri <marcelinogilbert26@gmail.com>
2022-08-10 16:03:54 +0800

    feat: theme toggle sound and hover tooltip

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -1,18 +1,54 @@
-import { useColorMode, IconButton, useColorModeValue } from "@chakra-ui/react";
+import {
+  useColorMode,
+  IconButton,
+  Button,
+  Tooltip,
+  Icon,
+} from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import useSound from "use-sound";
 
-export default function ColorModeSwitch({ ...props }): JSX.Element {
+export default function ColorModeSwitch({ mobile }): JSX.Element {
   const { colorMode, toggleColorMode } = useColorMode();
-
+  const [play] = useSound("/lightswitch.mp3", {
+    volume: 0.05,
+    sprite: {
+      on: [0, 300],
+      off: [500, 300],
+    },
+  });
+  const handleClick = () => {
+    toggleColorMode();
+    colorMode === "dark" ? play({ id: "on" }) : play({ id: "off" });
+  };
   return (
-    <IconButton
-      aria-label={colorMode === "dark" ? "Dark Mode" : "Light Mode"}
-      icon={colorMode === "dark" ? <MoonIcon /> : <SunIcon />}
-      isRound
-      variant="ghost"
-      onMouseDown={toggleColorMode}
-      mr={2}
-      {...props}
-    />
+    <Tooltip
+      label={colorMode === "dark" ? "Light Mode" : "Dark Mode"}
+      aria-label="A tool tip"
+    >
+      {mobile ? (
+        <Button
+          size="sm"
+          leftIcon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+          onClick={handleClick}
+        >
+          {colorMode === "dark" ? "Light Mode" : "Dark Mode"}
+        </Button>
+      ) : (
+        <IconButton
+          isRound
+          aria-label="Switch theme"
+          variant={mobile ? "ghost" : undefined}
+          icon={
+            colorMode === "dark" ? (
+              <Icon as={SunIcon} />
+            ) : (
+              <Icon as={MoonIcon} />
+            )
+          }
+          onClick={handleClick}
+        />
+      )}
+    </Tooltip>
   );
 }
