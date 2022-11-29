39f8d0190deb8ed8c810e813cfa269269f855b06
misterteri <marcelinogilbert26@gmail.com>
2022-11-29 15:23:44 +0800

    feat: bolder and lesser text

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,91 +9,38 @@ import {
   Spacer,
   Box,
   Link, // for external links
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
 import NiceAvatar, { genConfig } from "react-nice-avatar";
 import NextLink from "next/link"; // optimize for inside links within the same website
 import { AiTwotoneBoxPlot } from "react-icons/ai";
-// create object AvatarConfig
-const AvatarConfig = genConfig({
-  sex: "man",
-  faceColor: "#F9C9B6",
-  earSize: "big",
-  eyeStyle: "smile",
-  noseStyle: "long",
-  mouthStyle: "peace",
-  shirtStyle: "polo",
-  glassesStyle: "round",
-  hairColor: "#000",
-  hairStyle: "thick",
-  hatStyle: "none",
-  eyeBrowStyle: "up",
-  shirtColor: "#77311D",
-  bgColor: "#9FADC6",
-});
 
 const Home = (): JSX.Element => {
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
-
   return (
     <Layout>
-      {isMobile ? (
-        <>
-          <VStack spacing={8}>
-            <Heading as="h3" size="md">
-              <NiceAvatar
-                shape={"circle"}
-                style={{ width: "10rem", height: "10rem" }}
-                {...AvatarConfig}
-              />
-            </Heading>
-
-            <Box fontFamily={"helvetica"} gap={2}>
-              <Heading as="h1" color="#FF4500">
-                {"Marcelino Gilbert"}
-              </Heading>
-              <Text fontSize={"smaller"}>
-                An Undergraduate Student at{" "}
-                <Link
-                  href={"https://ibp.nthu.edu.tw/"}
-                  fontStyle={"italic"}
-                  fontWeight={"bold"}
-                  isExternal
-                >
-                  National Tsing Hua University
-                </Link>
-                .
-              </Text>
-            </Box>
-          </VStack>
-        </>
-      ) : (
-        <>
-          <HStack>
-            <Box>
-              <Heading as="h1">{"Marcelino Gilbert"}</Heading>
-              <Text fontSize={"smaller"}>
-                An Undergraduate Student at{" "}
-                <Link
-                  href={"https://ibp.nthu.edu.tw/"}
-                  fontStyle={"italic"}
-                  isExternal
-                >
-                  National Tsing Hua University
-                </Link>
-                .
-              </Text>
-            </Box>
-            <Spacer />
-            <NextLink href="/about">
-              <NiceAvatar
-                shape={"circle"}
-                style={{ width: "10rem", height: "10rem" }}
-                {...AvatarConfig}
-              />
-            </NextLink>
-          </HStack>
-        </>
-      )}
+      <Grid
+        height="100%"
+        templateRows="repeat(3, 2fr)"
+        templateColumns="repeat(3, 2fr)"
+      >
+        <GridItem rowSpan={3} colSpan={1} bg="black"></GridItem>
+        <GridItem rowSpan={1} colSpan={2}>
+          <Heading as="h1" size="4xl" fontWeight="bold" color="teal.500">
+            I'm Marcelino Gilbert.
+          </Heading>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={2}>
+          <Heading as="h1" size="4xl" fontWeight="bold" color="teal.500">
+            A Website Developer
+          </Heading>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={2}>
+          <Heading as="h1" size="4xl" fontWeight="bold" color="grey">
+            based in Taiwan.
+          </Heading>
+        </GridItem>
+      </Grid>
     </Layout>
   );
 };
